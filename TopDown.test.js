a56733fb6c6ebc252dc81c1249e2bf8c286f2a27
Roberto <rvega_22@alu.uabcs.mx>
2026-04-15 19:24:03 -0700

    TopDown Test

---
+++ TopDown.test.js
@@ -0,0 +1,46 @@
+const OrderController = require('./OrderController');
+// Aquí van a ir agregando los require conforme se vayan haciendo las pruebas
+// con los diferentes módulos
+
+test('Top-Down: fase 1', async () => {
+
+  // Uso de stub de OrderService para simular el comportamiento real
+  const orderServiceStub = {
+    createOrder: async () => ({
+      orderId: 1,
+      total: 110,
+      transactionId: 999
+    })
+  };
+
+  // Prueba de OrderController con el stub
+  const controller = new OrderController(orderServiceStub);
+
+  const result = await controller.createOrder({
+    userId: 1,
+    productId: 1,
+    quantity: 2,
+    price: 50
+  });
+
+  console.log(result);
+
+  // Validaciones con expect y matchers que ya han usado previamente
+  expect(result.status).toBe('success');
+  expect(result.data).toBeDefined();
+
+  // A partir de aquí comienzan con el Nivel 2
+  // Agrego ejemplo del stub del users
+
+  // Aquí deben completar los demás stubs:
+  // inventoryStub
+  // pricingStub
+  // paymentStub
+  // notificationStub
+
+  // Termiando nivel 2, comenzamos nivel 3, les agrego un ejemplo de
+  // OrderService, en este caso se nombra orderService3 porque ya se ha
+  // usado previamente un orderService y un orderService 2 en niveles previos
+
+  //Terminando nivel 3, comienzan aquí nivel 4 en donde NO se usa ningún stub
+});
